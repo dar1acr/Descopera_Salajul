a652a5355e7adc3cf030ffd1b23eb2fe8a3bea86
Daria <lou087lou3@gmail.com>
2025-09-13 19:32:40 +0300

    Trying to make it phone working

--- geography/scripts_source/index_script.js
+++ geography/scripts_source/index_script.js
@@ -1,4 +1,10 @@
-document.querySelectorAll('.marker').forEach(marker=>{
+document.querySelectorAll('.marker1').forEach(marker=>{
+    marker.addEventListener('click', ()=>{
+        alert('Character clicked!');
+    });
+});
+
+document.querySelectorAll('.marker2').forEach(marker=>{
     marker.addEventListener('click', ()=>{
         alert('Character clicked!');
     });
