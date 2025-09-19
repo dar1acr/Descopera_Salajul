2598aa5406466e1b4a6f416a5952c5c0a1f373a1
Daria <lou087lou3@gmail.com>
2025-09-19 22:01:46 +0300

    navbar_finished_phone+computer

--- geography/scripts_source/index.js
+++ geography/scripts_source/index.js
@@ -1,3 +1,14 @@
+//partea de scroll pentru meniu
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  }
+  else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+//partea de slide show
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,14 +24,15 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
+
